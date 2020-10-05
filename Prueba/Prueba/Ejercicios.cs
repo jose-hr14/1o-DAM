51fece4a455b3f4bfff8aca4aed5c4746c34dcc1
jose-hr14 <hernandez21107@iesmarenostrum.com>
2020-10-05 20:11:41 +0200

    +=1

---
+++ Prueba/Prueba/Ejercicios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba
+{
+    class Ejercicios
+    {
+        //Ejercicios: hacer una funcion que imprima por 
+        //pantalla los numeros del 0 al 99
+        public static void Ejercicio01()
+        {
+            int contador = 0;
+            while (contador < 100)
+            {
+                Console.WriteLine(contador);
+                contador += 1;
+            }
+        }
+
+    }
+}
--- Prueba/Prueba/Program.cs
+++ Prueba/Prueba/Program.cs
@@ -6,7 +6,7 @@ namespace Prueba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!!");
+            Ejercicios.Ejercicio01();
         }
     }
 }
