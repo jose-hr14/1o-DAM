5e4409860d51c152627245db96d09bf6e12bcc65
jose-hr14 <hernandez21107@iesmarenostrum.com>
2020-10-05 19:35:36 +0200

    Some utility classes
    From previus lessons

---
+++ Prueba/Prueba/Maths.cs
@@ -0,0 +1,22 @@
+﻿namespace FuncionTutorial
+{
+    class Maths
+    {
+        // Funcion 1
+        public static SolucionEcuacion CalcularResultado(double a, double b, double c)
+        {
+
+            double aux;
+            double solucion1;
+            double solucion2;
+            SolucionEcuacion solucion;
+            solucion = new SolucionEcuacion;
+            aux = (b * b) - (4 * a * c);
+            solucion1 = (-b + System.Math.Sqrt(aux)) / (2 * a);
+            solucion2 = (-b - System.Math.Sqrt(aux)) / (2 * a);
+
+            return solucion;
+        }
+
+    }
+}
---
+++ Prueba/Prueba/SolucionEcuacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FuncionTutorial
+{
+    class SolucionEcuacion
+    {
+        public double solucion1;
+        public double solucion2;
+
+    }
+}
