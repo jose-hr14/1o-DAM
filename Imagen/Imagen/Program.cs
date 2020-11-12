6e4fd4e858afa2f0b3b87973629d3ce14957ae9f
jose-hr14 <hernandez21107@iesmarenostrum.com>
2020-11-12 16:47:55 +0100

    Roman Face in progress

--- Imagen/Imagen/Program.cs
+++ Imagen/Imagen/Program.cs
@@ -17,14 +17,13 @@ namespace Imagen
         public static void PaintJapaneseFlag(Bitmap bm) //Cambiar el bnombre de bitmap por bm
         {
             //Hacer bandera japon, radio de 50, color rojo, la posicion de su centro es (250, 150)
+            FillImage(bm, Color.White);
+            FillCircle(bm, Color.Red, 250, 150, 50);
             for (int y = 0; y < bm.Height; y++)
             {
                 for (int x = 0; x < bm.Width; x++)
                 {
-                    if (GetDistance2d(250, 150, x, y) < 50)
-                    {
-                        bm.SetPixel(x, y, Color.Red);
-                    }
+                    
                     if (y == 0 || x == 0 || x == (bm.Width - 1) || y == (bm.Height - 1)) //Aquí coloreamos el borde negro
                     {
                         bm.SetPixel(x, y, Color.Black);
@@ -35,26 +34,68 @@ namespace Imagen
             
         }
 
+        //Vamos a poner un pixel rojo, usar esto para llenar el blaco de la bandera de japon
+        //bitmap.SetPixel(10, 10, Color.Red);
+        //Vamos a rellenar la img de rojo
+        //for (int y = 0; y < bitmap.Height; y++)
+        //{
+        //    for (int x = 0; x < bitmap.Width; x++)
+        //    {
+        //        bitmap.SetPixel(x, y, Color.White);
+        //    }
+        //}
+        public static void FillImage(Bitmap bm, Color Color)
+        {
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    bm.SetPixel(x, y, Color);
+                }
+            }
+        }
+
+        public static void FillCircle(Bitmap bm, Color color, double centerx, double centery, double radius)
+        {
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    if (GetDistance2d(centerx, centery, x, y) < radius)
+                    {
+                        bm.SetPixel(x, y, color);
+                    }
+                    
+                }
+            }
+
+        }
+
+        public static void PaintRomanFace(Bitmap bm)
+        {
+            FillImage(bm, Color.White);
+            FillCircle(bm, Color.Black, bm.Width / 2, bm.Height / 2, 120); //Cabeza externa
+            FillCircle(bm, Color.White, bm.Width / 2, bm.Height / 2, 110); //Cabeza borde interno
+            FillCircle(bm, Color.Black, bm.Width / 2.5, bm.Height / 3, 20); //Ojo izquieda
+            FillCircle(bm, Color.White, bm.Width / 2.5, bm.Height / 3, 10); //Pupila izquierda
+            //Ojo izquierdo
+            //Boca
+            FillCircle(bm, Color.Black, bm.Width / 2, bm.Height / 2, 10); //Nariz
+        }
+
         static void Main(string[] args)
         {
             Bitmap bitmap = new Bitmap(500, 300); //dimension completa de la imagen
+            PaintRomanFace(bitmap);
+            bitmap.Save("C:\\Users\\hernandez21107\\Desktop\\repos\\prueba.png");
 
 
-            //Vamos a poner un pixel rojo 
-            //bitmap.SetPixel(10, 10, Color.Red);
-            //Vamos a rellenar la img de rojo
-            //for (int y = 0; y < bitmap.Height; y++)
-            //{
-            //    for (int x = 0; x < bitmap.Width; x++)
-            //    {
-            //        bitmap.SetPixel(x, y, Color.White);
-            //    }
-            //}
 
 
             //Hacer bandera japon, radio de 50, color rojo, la posicion de su centro es (250, 150)
-            PaintJapaneseFlag(bitmap);
-            bitmap.Save("C:\\Users\\hernandez21107\\Desktop\\repos\\prueba.png");
+            //PaintJapaneseFlag(bitmap);
+            //Poner la funcion de cara roman
+
         }
     }
 }
