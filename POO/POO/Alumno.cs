c8b00a9b02ccffbd7be0cee6be04c459b78d4c96
jose-hr14 <hernandez21107@iesmarenostrum.com>
2020-12-02 14:59:08 +0200

    POO added

---
+++ POO/POO/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO
+{
+    class Alumno
+    {
+        private int nia; //lo de public es el nivel de visibilidad, si no ponemos, se declaran como private
+        private string nombre; //el private solo es visible desde dentro de la misma clase
+
+        public void InvalidateNia() //método de clase o de objeto, asi que no lleva static. Sin static, es un método de objeto, por tanto, necesita un objeto para utilizar dicho metodo.
+        {
+            nia = -1;
+        }
+
+        public void SetNia(int nia)
+        {
+            this.nia = nia;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        //this es la referencia al objeto con el que estamos realizando una operacion, this es una referencia
+    }
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Alumno alu = new Alumno();
+            alu.nia = 7;
+            alu.nombre = "Óscar";
+
+            Alumno alu2 = alu;
+            alu.nia = 8; //Como hacen referencia al mismo objeto, el nia cambia tanto en la variable alu como en la variable alu2
+
+            alu.InvalidateNia(); 
+
+            */
+
+            Alumno alu = new Alumno();
+            //alu.nia = 7;
+            //alu.nombre = "Aitor";
+            alu.SetNia(7); //Hemos puesto las propiedades como private, pero aun asi, podemos aisgnar las propiedades con los metodos de objeto de alumno, que si son publicos
+            alu.SetNombre("Aitor"); //Esto es porque los métodos de objeto sí pueden hacer a las propiedades de esa clase
+            Alumno lou = new Alumno();
+            
+        }
+    }
+}
