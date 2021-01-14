c40bf88b0e31e231b1c895d6e28b05863be45927
jose-hr14 <hernandez21107@iesmarenostrum.com>
2021-01-14 15:55:45 +0100

    Update Program.cs

--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sort
 {
@@ -6,7 +7,21 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Programa para manejar listas
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(-1);
+            list.Add(7);
+            list.Add(8);
+            list.Add(8);
+            list.Add(0);
+            list.Add(-5);
+
+            for (int i = 0; i < list.Count; i++) //imprime toda la lista
+            {
+                Console.WriteLine(list[i]);
+            }
+
         }
     }
 }
