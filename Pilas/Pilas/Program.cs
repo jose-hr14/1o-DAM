c435be1a03a8655cde256cbbb5d6e9449707fa8c
jose-hr14 <hernandez21107@iesmarenostrum.com>
2021-03-01 20:36:01 +0100

    Notacion Polaca

--- Pilas/Pilas/Program.cs
+++ Pilas/Pilas/Program.cs
@@ -43,9 +43,55 @@ namespace Pilas
                 }
             }
         }
+        public static Stack<int> CrearPilaNum()
+        {
+            Console.Write("Introduce el número de elementos que tendrá la pila: ");
+            int Count = Convert.ToInt32(Console.ReadLine());
+            Stack<int> pila = new Stack<int>();
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write("Introduce un número para la posición " + (i + 1) + " de la pila: ");
+                pila.Push(Convert.ToInt32(Console.ReadLine()));
+            }
+            return pila;
+        }
+        public static Queue<string> CrearPilaOperando()
+        {
+            Console.Write("Introduce el número de elementos que tendrá la cola: ");
+            int Count = Convert.ToInt32(Console.ReadLine());
+            Queue<string> cola = new Queue<string>();
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write("Introduce un operando para la posición " + (i + 1) + " de la cola: ");
+                cola.Enqueue(Console.ReadLine());
+            }
+            return cola;
+        }
+        public static int NotacionPolacaInversa(Stack<int> pila, Queue<string> cola)
+        {
+            int solucion = 0;
+            while (cola.Count > 0)
+            {
+                string operando = cola.Dequeue();
+                if (operando == "+")
+                    solucion = pila.Pop() + pila.Pop();
+                else if (operando == "-")
+                    solucion = pila.Pop() - pila.Pop();
+                else if (operando == "*")
+                    solucion = pila.Pop() * pila.Pop();
+                else if (operando == "/")
+                    solucion = pila.Pop() / pila.Pop();
+                pila.Push(solucion);
+            }
+            return pila.Pop();
+        }
         static void Main(string[] args)
         {
-            PilaEscaleraAsteriscos(6);
+            //PilaEscaleraAsteriscos(6);
+            //CrearPilaNum();
+            //CrearPilaOperando();
+            NotacionPolacaInversa(CrearPilaNum(), CrearPilaOperando());
+
         }
     }
 }
