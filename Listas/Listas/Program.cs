f3c86886951bd08eb48e0c4154db239271bba886
jose-hr14 <hernandez21107@iesmarenostrum.com>
2021-03-11 16:45:17 +0100

    Update Program.cs

--- Listas/Listas/Program.cs
+++ Listas/Listas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 
 namespace Listas
 {
@@ -12,10 +13,11 @@ namespace Listas
                 Console.WriteLine(Lista[i]);
             }
         }
-        static void Main(string[] args)
+
+        public static void TeoriaClase01()
         {
             ArrayList Lista = new ArrayList();
-            
+
             Console.WriteLine("El número de elementos es " + Lista.Count);
             Lista.Add(2); //Metemos elementos en la lista
             Console.WriteLine("El número de elementos es " + Lista.Count);
@@ -65,7 +67,7 @@ namespace Listas
             Lista02.Add(3);
             Lista02.Add(3);
             //for (int i = 0; i < Lista.Count; i++) //No podriamos borrar todos los 3 de la lista si la lista solo tiene 3
-                //Lista02.Remove(3);
+            //Lista02.Remove(3);
             Console.WriteLine();
 
             int Count = Lista02.Count;
@@ -79,6 +81,86 @@ namespace Listas
 
             //Lista02.Contains(2); //Bool que devuelve si el elemento está en la lista o no
             //Lista02.BinarySearch(2); //Devuelve la dirección donde se encuentra el dato, pero si no lo encuentra, devuelve -1
+        }
+
+        public static void ImprimirMenu()
+        {
+            Console.WriteLine("1.- Introducir un nuevo préstamo.");
+            Console.WriteLine("2.- Actualizar préstamo.");
+            Console.WriteLine("3.- Cancelar préstamo.");
+            Console.WriteLine("4.- Listar todos los préstamos.");
+            Console.WriteLine("5.- Listar todos los préstamos según cantidad adeudada, amortizados y cancelados.");
+        }
+        public static void SumarAlArray(int[] Lista) //Funcion para comprobar que los tipos de datos compuestos se pasan por referencia automáticamente
+        {
+            Lista[0] += 2;
+        }
+        public static void PruebaSumarAlArray()
+        {
+            int[] numero = { 1, 2, 3, 4 };
+            SumarAlArray(numero);
+        }
+        static void Main(string[] args)
+        {
+            //ImprimirMenu();
+            
+            int option = 5;
+            switch (option)
+            {
+                case 1:                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+            char[] LetraDNI =  {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F','P', 'D', 'X', 'B', 'N'};
+            pedir:
+            Console.WriteLine("Introduce el DNI: ");
+            string DNI = Console.ReadLine();
+            char letra = char.ToUpper(DNI[8]); //Sacamos asi letra de dni para hacer la verificacion
+            
+            string numero = DNI.Remove(8, 1); //Sacar los numeros del dni
+
+            int j = 0;
+            bool es_numero = int.TryParse(numero, out j); //comprobar que es un numero
+
+            if (!LetraDNI.Contains(letra))//comprobar letra, si no, reinicia
+            {
+                Console.WriteLine("mensaje error");
+                goto pedir;
+            }
+            else if (!es_numero) //comprueba que son numeros,si no, reinciia. Retrocede segun el valor booleano de es_numero
+            {
+                Console.WriteLine("error");
+                goto pedir;
+            }
+            else if (DNI.Length != 9) //Comprobar que la longitud del dni introducido es el correcto
+            {
+                Console.WriteLine("Error");
+                goto pedir;
+            }
+
+
+
+            int num = Convert.ToInt32(numero);//Pasamos el numero a int
+            int pos = num % 23;
+            if (LetraDNI[pos]  != letra)
+            {
+                Console.WriteLine("Error");
+                goto pedir;
+            }
+            else
+            {
+                DNI = numero + letra;
+            }
+                
+
         }
     }
 }
