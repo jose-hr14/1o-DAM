2a3ca8d78ee2d986e0fa8620321a57da7748185c
jose-hr14 <hernandez21107@iesmarenostrum.com>
2021-03-29 19:32:04 +0200

    Tercer trimestre

---
+++ CorrecionExamenEv02/CorrecionExamenEv02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CorrecionExamenEv02
+{
+    class Program
+    {
+        public static void Ej01()
+        {
+            int[] Array01 = new int[] { 3, 6, 7, 9, 5 };
+            int[] Array02 = new int[] { 5, 6, 8, 7, 5 };
+            bool esta = false;
+            for (int i = 0; i < Array01.Length; i++)
+            {
+                for (int j = 0; j < Array02.Length; j++)
+                {
+                    if (Array01[i] == Array02[j])
+                    {
+                        esta = true;
+                    }
+                    else
+                    {
+                        esta = false;
+                    }
+
+                }
+                if (!esta)
+                {
+                    Console.WriteLine("El numero no esta");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Objetos/Objetos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Objetos
+{
+    class Program
+    {
+        // Las lineas de codigo para crear un vector de enteros de 10 elementos, nos tiene que decir si el numero introducido está en el vector o no
+        public static void EjProfe()
+        {
+            int[] Vector = new int[10] { 1, 7, 9, 8, 6, 11, 12, 13, 14, 5 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                if (Vector[i] == num)
+                {
+                    Console.WriteLine("El número está");
+                    break;
+                }
+                    
+                if (i == Vector.Length - 1)
+                    Console.WriteLine("El número no está");
+            }
+        }
+        static void Main(string[] args)
+        {
+            EjProfe();
+        }
+    }
+}
