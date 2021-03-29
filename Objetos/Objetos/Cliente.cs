0e719567022fcd4cf77270782950bf5b7c580c7e
jose-hr14 <hernandez21107@iesmarenostrum.com>
2021-03-29 20:40:09 +0200

    clase cliente creada

---
+++ Objetos/Objetos/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objetos
+{
+    class Cliente
+    {
+        private string nombre;
+        private string apellidos;
+        private int edad;
+        private string dni;
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetApellidos(string apellidos)
+        {
+            this.apellidos = apellidos;
+        }
+        public string GetApellidos()
+        {
+            return apellidos;
+        }
+        public void SetEdad(int edad)
+        {
+            this.edad = edad;
+        }
+        public int GetEdad()
+        {
+            return edad;
+        }
+        public void SetDNI(string dni)
+        {
+            this.dni = dni;
+        }
+        public string GetDNI()
+        {
+            return dni;
+        }
+    }
+}
--- Objetos/Objetos/Program.cs
+++ Objetos/Objetos/Program.cs
@@ -21,9 +21,29 @@ namespace Objetos
                     Console.WriteLine("El número no está");
             }
         }
+        public static void EjProfeSolucion()
+        {
+            int[] Vector = new int[10] { 1, 7, 9, 8, 6, 11, 12, 13, 14, 5 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                if (Vector[i] == num)
+                {
+                    Console.WriteLine("El número está");
+                    break;
+                }
+                else if (i == Vector.Length - 1)
+                    Console.WriteLine("El número no está");
+            }
+        }
         static void Main(string[] args)
         {
-            EjProfe();
+            //EjProfe();
+            Cliente profe_juanfran = new Cliente();
+            profe_juanfran.nombre = "Juan Francisco";
+            profe_juanfran.apellidos = "Bueno";
+            profe_juanfran.edad = 45;
+            profe_juanfran.dni = "123456789";
         }
     }
 }
