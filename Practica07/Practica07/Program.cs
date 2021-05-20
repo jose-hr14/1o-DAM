1e427be56e2be4883039fe5bf1ab77ad57abc92c
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-05-20 21:05:35 +0200

    práctica acabada, queda refinar

--- Practica07/Practica07/Program.cs
+++ Practica07/Practica07/Program.cs
@@ -6,17 +6,51 @@ namespace Practica07
 {
     class Program
     {
+        public static void EnterParaContinuar()
+        {
+            Console.Write("Pulsa enter para continuar: ");
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
             tListadeAlumnos listaAlumnos = new tListadeAlumnos();
             tListadeProfesores listaProfesores = new tListadeProfesores();
             tListadeCursos listaCursos = new tListadeCursos();
 
-            //Gestion_Cursos.MenuPrincipal(listaCursos, listaAlumnos);
-            //GestionAlumnos.MenuPrincipal(listaCursos, listaAlumnos);
-            GestionProfesores.MenuPrincipal(listaProfesores);
-
-            
+            string opcion = "";
+            bool salir = false;
+            do
+            {
+                Console.WriteLine("-- Gestión Instituto --");
+                Console.WriteLine("1.- Gestión cursos");
+                Console.WriteLine("2.- Gestión alumnos");
+                Console.WriteLine("3.- Gestión profesores");
+                Console.WriteLine("0.- Salir");
+                Console.Write("Elige una opción: ");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.Clear();
+                        Gestion_Cursos.MenuPrincipal(listaCursos, listaAlumnos);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        GestionAlumnos.MenuPrincipal(listaCursos, listaAlumnos);
+                        break;
+                    case "3":
+                        Console.Clear();
+                        GestionProfesores.MenuPrincipal(listaProfesores);
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida");
+                        break;
+                }
+                EnterParaContinuar();
+                Console.Clear();
+            } while (salir == false);            
         }
     }
 }
