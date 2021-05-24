e3ece96fdc2dcc36023f9370264bf85f6fe76015
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-05-24 17:38:53 +0200

    Update Practica Cursores 02.sql

--- Consultas SQL/Practica Cursores 02.sql
+++ Consultas SQL/Practica Cursores 02.sql
@@ -15,7 +15,8 @@ WHILE (@@FETCH_STATUS = 0)
 		FETCH NEXT FROM Cur_Producto INTO @nombre
 	END
 CLOSE Cur_Producto;
-DEALLOCATE Cur_Producto;
+DEALLOCATE Cur_Producto;
+
 --2.- Nombre y precio de productos en BD TIENDA
 --Ampliación del script anterior. Utilizando la BD TIENDA, crea un script que recorra con
 --cursores la tabla PRODUCTO y que muestre el nombre de cada producto y su precio por
@@ -63,21 +64,23 @@ FETCH NEXT FROM Cur_Fabricante INTO @codigo;
 WHILE (@@FETCH_STATUS = 0)
 	BEGIN
 		-- Inicio del segundo cursor 
-		DECLARE Cur_Fabricante CURSOR FOR
+		DECLARE Cur_Fabricante02 CURSOR FOR
 			SELECT codigo
 			FROM PRODUCTO
 			WHERE codigo_fabricante = @codigo;
-		DECLARE @codigo INT, @nombre VARCHAR(100), @precio NUMERIC(9,2);
+		DECLARE @codigoProducto INT, @total NUMERIC(9,2);
+		SET @total = 0;
 
-		OPEN Cur_Fabricante;
-		FETCH NEXT FROM Cur_Fabricante INTO @codigo;
+		OPEN Cur_Fabricante02;
+		FETCH NEXT FROM Cur_Fabricante02 INTO @codigoProducto;
 		WHILE (@@FETCH_STATUS = 0)
 			BEGIN
-		
-				FETCH NEXT FROM Cur_Fabricante INTO @codigo;
+				SET @total += (SELECT precio FROM PRODUCTO WHERE codigo = @codigoProducto);
+				FETCH NEXT FROM Cur_Fabricante02 INTO @codigoProducto;
 			END
-		CLOSE Cur_Fabricante;
-		DEALLOCATE Cur_Fabricante;
+		PRINT @total;
+		CLOSE Cur_Fabricante02;
+		DEALLOCATE Cur_Fabricante02;
 		-- Fin del segundo cursor
 		FETCH NEXT FROM Cur_Fabricante INTO @codigo;
 	END
@@ -87,7 +90,26 @@ DEALLOCATE Cur_Fabricante;
 --Crea un script que recorra con cursores la tabla EMPLEADOS y que muestre los siguientes
 --datos de cada empleado: nombre, apellido1, apellido2 y email. Deberás formatearlos
 --utilizando la función CONCAT.
+USE JARDINERIA;
+GO
+DECLARE Cur_Empleados CURSOR FOR
+	SELECT codEmpleado
+	FROM EMPLEADOS;
+DECLARE @codigoEmpleado INT, @nombre VARCHAR(50), @apellido1 VARCHAR(50), @apellido2 VARCHAR(50), @email VARCHAR(100);
 
+OPEN Cur_Empleados;
+FETCH NEXT FROM Cur_Empleados INTO @codigoEmpleado;
+WHILE (@@FETCH_STATUS = 0)
+	BEGIN
+		SET @nombre = (SELECT nombre FROM EMPLEADOS WHERE codEmpleado = @codigoEmpleado);
+		SET @apellido1 = (SELECT apellido1 FROM EMPLEADOS WHERE codEmpleado = @codigoEmpleado);
+		SET @apellido2 = (SELECT apellido2 FROM EMPLEADOS WHERE codEmpleado = @codigoEmpleado);
+		SET @email = (SELECT email FROM EMPLEADOS WHERE codEmpleado = @codigoEmpleado);
+		PRINT CONCAT ('Nombre: ', @nombre, ' ', @apellido1, ' ', @apellido2, ' | Email: ', @email);
+		FETCH NEXT FROM Cur_Empleados INTO @codigoEmpleado;
+	END
+CLOSE Cur_Empleados;
+DEALLOCATE Cur_Empleados;
 
 --5.- Cursores anidados en BD JARDINERIA
 --En este script deberás utilizar un cursor que recorra la tabla PEDIDOS y dentro de éste
